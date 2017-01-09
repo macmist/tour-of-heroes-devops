24622cf2cc6f3f123aad486aff2256e9be9e5041
Quentin MUSY <musy_q@epita.fr>
2017-01-09 18:40:53 +0100

    [HOOK] add precommit hook

--- angular/app/hero-search.component.ts
+++ angular/app/hero-search.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router }            from '@angular/router';
 import { Observable }        from 'rxjs/Observable';
 import { Subject }           from 'rxjs/Subject';
-import { HeroSearchService } from './hero-search.service';
+import { HeroSearchService } from './hero-search.service' 
 
 import { Hero } from './hero';
 @Component({
@@ -12,6 +12,7 @@ import { Hero } from './hero';
   styleUrls: [ 'hero-search.component.css' ],
   providers: [HeroSearchService]
 })
+
 export class HeroSearchComponent implements OnInit {
   heroes: Observable<Hero[]>;
   private searchTerms = new Subject<string>();
